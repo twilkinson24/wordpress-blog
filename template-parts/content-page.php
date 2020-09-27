d0eb3317f6d5e2f8f0440cf20ef73c2ea7495ead
Taylor <taylorwteaches@gmail.com>
2020-09-27 09:56:44 -0400

    container additions to default pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -42,46 +42,55 @@ $featured_img_url = get_the_post_thumbnail_url($post->ID,'large');
 
 
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<?php echo 'original ft image was here'; // _s_post_thumbnail(); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
 
-	<div class="entry-content">
-		<?php
-		the_content();
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+				<?php echo 'original ft image was here'; // _s_post_thumbnail(); ?>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
+				<div class="entry-content">
+					<?php
+					the_content();
+
+					wp_link_pages(
 						array(
-							'span' => array(
-								'class' => array(),
-							),
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+							'after'  => '</div>',
 						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+					);
+					?>
+				</div><!-- .entry-content -->
+
+				<?php if ( get_edit_post_link() ) : ?>
+					<footer class="entry-footer">
+						<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								wp_kses_post( get_the_title() )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+						?>
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+		</div> <!-- end .col -->
+	</div><!-- end .row -->
+</div> <!-- end .container -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -22,7 +22,7 @@ if(has_post_thumbnail($post->ID)) :
 $featured_img_url = get_the_post_thumbnail_url($post->ID,'large');
 ?>
 <div class="hero position-relative">
-	<div class="hero-overlay h-100 w-100" style="background-image: url('<?php echo $featured_img_url; ?>');'">
+	<div class="hero-overlay h-100 w-100" style="background-image: url('<?php echo $featured_img_url; ?>');">
 		<div class="container">
 			<h1 class="page-title"><?php the_title(); ?></h1> 
 		</div>
@@ -41,57 +41,79 @@ $featured_img_url = get_the_post_thumbnail_url($post->ID,'large');
 
 <?php endif; ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				_s_posted_on();
-				_s_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+<main id="primary" class="site-main">
+			<div class="container">
+				<div class="row">
 
-	<?php echo 'original featured image was here'; //_s_post_thumbnail(); ?>
+				<?php if ( is_active_sidebar( 'blog-sidebar' ) ) : ?>
+					<div class="col-lg-8">
+				<?php else : ?>
+					<div class="col-12">
+				<?php endif; ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+								<?php
+								if ( is_singular() ) :
+									the_title( '<h1 class="entry-title">', '</h1>' );
+								else :
+									the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+								endif;
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+								if ( 'post' === get_post_type() ) :
+									?>
+									<div class="entry-meta">
+										<?php
+										_s_posted_on();
+										_s_posted_by();
+										?>
+									</div><!-- .entry-meta -->
+								<?php endif; ?>
+							</header><!-- .entry-header -->
 
-	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+							<?php echo 'original featured image was here'; //_s_post_thumbnail(); ?>
 
+							<div class="entry-content">
+								<?php
+								the_content(
+									sprintf(
+										wp_kses(
+											/* translators: %s: Name of current post. Only visible to screen readers */
+											__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+											array(
+												'span' => array(
+													'class' => array(),
+												),
+											)
+										),
+										wp_kses_post( get_the_title() )
+									)
+								);
+
+								wp_link_pages(
+									array(
+										'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+										'after'  => '</div>',
+									)
+								);
+								?>
+							</div><!-- .entry-content -->
+
+							<footer class="entry-footer">
+								<?php _s_entry_footer(); ?>
+							</footer><!-- .entry-footer -->
+						</article><!-- #post-<?php the_ID(); ?> -->
+
+					</div> <!-- end .col -->
+
+					<!-- sidebar -->
+					<?php 	if ( is_active_sidebar( 'blog-sidebar' ) ) : ?>
+						<aside class="blog-sidebar col-lg-4">
+							<?php dynamic_sidebar( 'blog-sidebar' ); ?>
+						</aside>						
+					<?php endif; ?>
+
+				</div> <!-- end .row -->
+			</div> <!-- end .container -->
 <?php 
